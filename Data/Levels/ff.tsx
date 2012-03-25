c1c72a733a555f1c8f889e881657abc985694740
xander.deseyn <xander.deseyn@3d8eb7fa-c818-4c5a-1134-f91773aba49e>
2012-03-25 16:15:37 +0000

    Added data

---
+++ Data/Levels/ff.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ff" tilewidth="32" tileheight="32" spacing="2" margin="2">
+ <image source="../Images/tileset.png" width="546" height="376"/>
+ <tile id="130">
+  <properties>
+   <property name="platform" value=""/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="platform" value=""/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="platform" value=""/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="platform" value=""/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="coin" value=""/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="transparent" value=""/>
+  </properties>
+ </tile>
+ <tile id="149">
+  <properties>
+   <property name="transparent" value=""/>
+  </properties>
+ </tile>
+</tileset>
